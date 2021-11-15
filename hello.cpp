520427d265d7ac92996c37179dc9bb4209417a4a
Gustavo Marques <gustavo.siqueira.marques@gmail.com>
2021-11-15 14:16:53 +0100

    First commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main() {
+   string name = "World";
+
+   cout << "Hello " + name; // prints Hello World
+   return 0;
+}
